0e8091e307672da6e68d5877bdd5a390412c3788
Biru Mohanathas <birunthan@mohanathas.com>
2024-03-28 10:57:44 +0200

    Add workflow for GitHub Pages deploy

--- ci-build.sh
+++ ci-build.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o pipefail
 
-ZOLA_VERSION=0.17.2
+ZOLA_VERSION=0.18.0
 wget --quiet "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 tar xf "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 ./zola build
